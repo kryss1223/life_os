f750b249bf5b1baba196b62c2974afd7865c68db
Antonio Prieto <antonioprieto2207@gmail.com>
2026-08-24 16:58:21 +0200

    Add login user

--- config/settings.py
+++ config/settings.py
@@ -158,3 +158,10 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+
+LOGIN_URL = "/accounts/login/"
+
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/accounts/login/"
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,11 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path(
+        "accounts/",
+        include("django.contrib.auth.urls"),
+    ),
+
     path("", include("life.urls")),
 ]
